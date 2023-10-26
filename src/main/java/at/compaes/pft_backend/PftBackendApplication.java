e7153982afbe86572166b8471198c2a1c04e8b57
David <david281100@hotmail.com>
2023-10-26 16:18:01 +0200

    init commit

---
+++ src/main/java/at/compaes/pft_backend/PftBackendApplication.java
@@ -0,0 +1,13 @@
+package at.compaes.pft_backend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PftBackendApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(PftBackendApplication.class, args);
+    }
+
+}
---
+++ src/test/java/at/compaes/pft_backend/PftBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package at.compaes.pft_backend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PftBackendApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
