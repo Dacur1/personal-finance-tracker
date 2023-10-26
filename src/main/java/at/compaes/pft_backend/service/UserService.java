3ada2999f5eedfa34269894413f0b2ecaf57d09a
David <david281100@hotmail.com>
2023-10-26 16:51:48 +0200

    created user service

---
+++ src/main/java/at/compaes/pft_backend/service/UserService.java
@@ -0,0 +1,22 @@
+package at.compaes.pft_backend.service;
+
+import at.compaes.pft_backend.model.User;
+import at.compaes.pft_backend.repository.UserRepository;
+import lombok.AllArgsConstructor;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+@Service
+public class UserService {
+    @Autowired
+    public UserService(UserRepository userRepository) {
+        this.userRepository = userRepository;
+    }
+
+    private final UserRepository userRepository;
+
+    public User findByUsername(String username) {
+        return userRepository.findByUsername(username);
+    }
+
+}
